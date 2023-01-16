8700a58c7ef58150c32faf7133dcff2f68852f21
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-16 16:38:59 +0200

    Init commit

---
+++ src/test/java/com/btrco/springbootlibrary/SpringBootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btrco.springbootlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
