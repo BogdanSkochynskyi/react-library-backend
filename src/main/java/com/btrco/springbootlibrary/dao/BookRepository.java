9603fd88efc5a7a76381750bbfed3f2edc2b24f2
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-18 13:21:32 +0200

    Add find by category repository method

--- src/main/java/com/btrco/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/btrco/springbootlibrary/dao/BookRepository.java
@@ -9,4 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
+
 }
