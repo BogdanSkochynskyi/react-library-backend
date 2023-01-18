87653dabcdf93f282573115dbc33956f880c0198
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-18 12:58:39 +0200

    Add find by title repository method

--- src/main/java/com/btrco/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/btrco/springbootlibrary/dao/BookRepository.java
@@ -1,7 +1,12 @@
 package com.btrco.springbootlibrary.dao;
 
 import com.btrco.springbootlibrary.entitiy.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
