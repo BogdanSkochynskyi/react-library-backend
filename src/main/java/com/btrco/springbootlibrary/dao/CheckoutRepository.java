4288b1533d1a40dfd383256b7ac4147fd7c07068
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-20 15:25:01 +0200

    Add security configuration

--- src/main/java/com/btrco/springbootlibrary/dao/CheckoutRepository.java
+++ src/main/java/com/btrco/springbootlibrary/dao/CheckoutRepository.java
@@ -1,10 +1,7 @@
 package com.btrco.springbootlibrary.dao;
 
 import com.btrco.springbootlibrary.entitiy.Checkout;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
