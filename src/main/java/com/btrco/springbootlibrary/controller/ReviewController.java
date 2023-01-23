5a80e3c0eec74eccd32983cb42c7a5c08d8632ce
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-23 13:10:20 +0200

    Added an review functionality

---
+++ src/main/java/com/btrco/springbootlibrary/controller/ReviewController.java
@@ -0,0 +1,39 @@
+package com.btrco.springbootlibrary.controller;
+
+import com.btrco.springbootlibrary.requestModels.ReviewRequest;
+import com.btrco.springbootlibrary.service.ReviewService;
+import com.btrco.springbootlibrary.utils.ExtractJWT;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping("/secure/user/book")
+    public Boolean reviewBookByUser(@RequestHeader(value = "Authorization") String token,
+                                    @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        return reviewService.userReviewListed(userEmail, bookId);
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody ReviewRequest reviewRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
---
+++ src/main/java/com/btrco/springbootlibrary/requestModels/ReviewRequest.java
@@ -0,0 +1,15 @@
+package com.btrco.springbootlibrary.requestModels;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
+public class ReviewRequest {
+
+    private double rating;
+
+    private Long bookId;
+
+    private Optional<String> reviewDescription;
+}
