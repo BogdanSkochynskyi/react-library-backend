45348b52037e5300a0fd730c3e14357d875469dd
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-24 18:00:28 +0200

    History added

--- src/main/java/com/btrco/springbootlibrary/service/BookService.java
+++ src/main/java/com/btrco/springbootlibrary/service/BookService.java
@@ -2,8 +2,10 @@ package com.btrco.springbootlibrary.service;
 
 import com.btrco.springbootlibrary.dao.BookRepository;
 import com.btrco.springbootlibrary.dao.CheckoutRepository;
+import com.btrco.springbootlibrary.dao.HistoryRepository;
 import com.btrco.springbootlibrary.entitiy.Book;
 import com.btrco.springbootlibrary.entitiy.Checkout;
+import com.btrco.springbootlibrary.entitiy.History;
 import com.btrco.springbootlibrary.responseModels.ShelfCurrentLoansResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,9 +26,12 @@ public class BookService {
 
     private CheckoutRepository checkoutRepository;
 
-    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+    private HistoryRepository historyRepository;
+
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository, HistoryRepository historyRepository) {
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
+        this.historyRepository = historyRepository;
     }
 
     public Book checkoutBook(String userEmail, Long bookId) throws Exception {
@@ -105,6 +110,10 @@ public class BookService {
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
+
+        History history = new History(userEmail, validateCheckout.getCheckoutDate(), LocalDate.now().toString(),
+                book.get().getTitle(), book.get().getAuthor(), book.get().getDescription(), book.get().getImg());
+        historyRepository.save(history);
     }
 
     public void renewLoan(String userEmail, Long bookId) throws Exception {
