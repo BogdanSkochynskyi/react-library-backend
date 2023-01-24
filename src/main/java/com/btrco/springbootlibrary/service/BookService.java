d7ba4552c10ad4bb1f2475c0611b2920c6be031b
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-24 17:11:48 +0200

    Added renew loan endpoint

--- src/main/java/com/btrco/springbootlibrary/service/BookService.java
+++ src/main/java/com/btrco/springbootlibrary/service/BookService.java
@@ -73,20 +73,21 @@ public class BookService {
 
         List<Book> books = bookRepository.findBooksByBookIds(bookIdList);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
         for (Book book: books) {
             Optional<Checkout> checkout = checkoutList.stream().filter(x-> x.getBookId() == book.getId()).findFirst();
 
             if (checkout.isPresent()) {
+
                 Date d1 = sdf.parse(checkout.get().getReturnDate());
                 Date d2 = sdf.parse(LocalDate.now().toString());
 
                 TimeUnit time = TimeUnit.DAYS;
 
-                long differenceInTime = time.convert(d1.getTime()-d2.getTime(), TimeUnit.MICROSECONDS);
+                long difference_In_Time = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
 
-                shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) differenceInTime));
+                shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) difference_In_Time));
             }
         }
         return shelfCurrentLoansResponses;
@@ -104,5 +105,21 @@ public class BookService {
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
+    }
+
+    public void renewLoan(String userEmail, Long bookId) throws Exception {
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if (validateCheckout == null) {
+            throw new Exception("Book does not exists or not checked out by user");
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date d1 = sdf.parse(validateCheckout.getReturnDate());
+        Date d2 = sdf.parse(LocalDate.now().toString());
+
+        if (d1.compareTo(d2) > 0 || d1.compareTo(d2) == 0) {
+            validateCheckout.setReturnDate(LocalDate.now().plusDays(7).toString());
+            checkoutRepository.save(validateCheckout);
+        }
     }
 }
